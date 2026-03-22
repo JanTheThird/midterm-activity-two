41f594bb8e4cbd146398ebb19e03646f1be02eb8
JanTheThird <20122613@s.ubaguio.edu>
2026-03-22 20:14:51 +0800

    Created navigation component and setup the router.

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,39 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
+import Register from './pages/Register.jsx'
+import Student from './pages/Student.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "home",
+        element: <Home />
+      },
+      {
+        path: "register",
+        element: <Register />
+      },
+      {
+        path: "student/:id",
+        element: <Student />
+      }
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import StudentList from "../components/StudentList"
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <StudentList />
+    </div>
+  )
+}
+
+export default Home
